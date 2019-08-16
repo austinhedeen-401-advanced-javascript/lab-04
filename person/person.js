0c4d2cdac03ad9bd1161a7d56f6100241e609e21
Austin Hedeen <austin.hedeen@gmail.com>
2019-08-16 09:29:32 -0700

    Implement person model

--- person/person.js
+++ person/person.js
@@ -1,1 +1,29 @@
-// TODO - Create a new data model that uses the file model type
+'use strict';
+
+const DataModel = require('../file.js');
+
+class Person extends DataModel {
+  constructor() {
+    super('person');
+    this.schema = {
+      id: {
+        required: true,
+        type: 'string',
+      },
+      first_name: {
+        required: true,
+        type: 'string',
+      },
+      last_name: {
+        required: true,
+        type: 'string',
+      },
+      age: {
+        required: true,
+        type: 'number',
+      },
+    };
+  }
+}
+
+module.exports = Person;
