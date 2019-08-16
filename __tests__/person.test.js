b3f46015bcee5ec21dfa58dcd6fe9b8a99239de7
Austin Hedeen <austin.hedeen@gmail.com>
2019-08-16 09:40:02 -0700

    Implement tests for create() and get() person

--- __tests__/person.test.js
+++ __tests__/person.test.js
@@ -1,1 +1,42 @@
-// TODO - write a full test suite, roughly reflecting memory.js
+'use strict';
+
+const Person = require('../person/person');
+
+describe('Person Model', () => {
+
+  const person = new Person('person');
+  const testPerson = {
+    first_name: 'Austin',
+    last_name: 'Hedeen',
+    age: 27,
+  };
+
+  beforeEach(() => {
+    return person.init()
+      .then()
+      .catch(error => console.error(error));
+  });
+
+  test('can create() a new person', () => {
+    return person.create(testPerson)
+      .then(record => {
+        Object.keys(testPerson).forEach(key => {
+          expect(record[key]).toEqual(testPerson[key]);
+        });
+      })
+      .catch(error => console.error(error));
+  });
+
+  test('can get() a person', () => {
+    return person.create(testPerson)
+      .then(record => {
+        return person.get(record.id);
+      })
+      .then(record => {
+        Object.keys(testPerson).forEach(key => {
+          expect(record[0][key]).toEqual(testPerson[key]);
+        });
+      })
+      .catch(error => console.error(error));
+  });
+});
